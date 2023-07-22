bf0c24bf18c7ae7d885e7cd6cf1b2d65b7bdb519
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-07-22 23:27:24 +0530

    updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,4 +9,38 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.7",
+  defaultNetwork: "hardhat",
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_MATIC_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80001,
+      blockConfirmations: 6,
+      gas: 6000000,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+    token: "ETH",
+    coinmarketcap: process.env.COINCAPMARKET_API_KEY,
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY,
+      polygonMumbai: process.env.POLYGON_API_KEY,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
 };
